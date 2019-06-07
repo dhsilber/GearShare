67594308b30239b133ef25fdaad44f61ca9d2221
David H. Silber <davidhsilber@gmail.com>
2019-06-07 18:56:12 -0400

    0.0.0-6 Cleaned up tests.

--- src/main/java/name/davidsilber/theater/share/TemplateViewControllers.java
+++ src/main/java/name/davidsilber/theater/share/TemplateViewControllers.java
@@ -5,13 +5,13 @@ import org.springframework.context.annotation.Configuration;
         import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class TemplateViewControllers implements WebMvcConfigurer {
+class TemplateViewControllers implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
 //        registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("main");
+        registry.addViewController("/").setViewName("root");
         registry.addViewController("/admin").setViewName("admin");
-        registry.addViewController("/login").setViewName("login");
+//        registry.addViewController("/login").setViewName("login");
     }
 
 }
--- src/test/java/name/davidsilber/theater/share/RootPageContentTests.java
+++ src/test/java/name/davidsilber/theater/share/RootPageContentTests.java
@@ -2,12 +2,11 @@ package name.davidsilber.theater.share;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -18,14 +17,15 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(value = TemplateViewControllers.class, secure = false)
-public class MainPageContentTest {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class RootPageContentTests {
 
     @Autowired
     private MockMvc mockMvc;
 
     MvcResult result;
 
-    @BeforeEach
+    @BeforeAll
     public void setup() throws Exception {
         System.out.println("Beginning------------");
         RequestBuilder requestBuilder = MockMvcRequestBuilders
@@ -36,48 +36,21 @@ public class MainPageContentTest {
                 .perform(requestBuilder)
                 .andReturn();
 
-        System.out.println("Start:"+ result.getResponse().getContentAsString()+":End");
+//        System.out.println("Start:"+ result.getResponse().getContentAsString()+":End");
     }
 
     @Test
-    public void mainPageIncludesLoginFormTest() throws Exception {
-        RequestBuilder requestBuilder = MockMvcRequestBuilders
-                .get("/")
-                .accept(MediaType.TEXT_HTML);
-
-        result = mockMvc
-                .perform(requestBuilder)
-                .andReturn();
-
-        System.out.println("Start:"+ result.getResponse().getContentAsString()+":End");
+    public void mainPageIncludesLoginForm() throws Exception {
         assertTrue(result.getResponse().getContentAsString().contains("<form action=\"/login\" method=\"post\">"));
     }
 
     @Test
-    public void mainPageIncludesUserNameInputtest() throws Exception {
-//        RequestBuilder requestBuilder = MockMvcRequestBuilders
-//                .get("/")
-//                .accept(MediaType.TEXT_HTML);
-//
-//        result = mockMvc
-//                .perform(requestBuilder)
-//                .andReturn();
-//
-//        System.out.println("Start:"+ result.getResponse().getContentAsString()+":End");
+    public void mainPageIncludesUserNameInput() throws Exception {
         assertTrue(result.getResponse().getContentAsString().contains("<input type=\"text\" name=\"username\"/>"));
     }
 
     @Test
-    public void testmainPageIncludesPasswordInput() throws Exception {
-//        RequestBuilder requestBuilder = MockMvcRequestBuilders
-//                .get("/")
-//                .accept(MediaType.TEXT_HTML);
-//
-//        result = mockMvc
-//                .perform(requestBuilder)
-//                .andReturn();
-//
-//        System.out.println("Start:"+ result.getResponse().getContentAsString()+":End");
+    public void mainPageIncludesPasswordInput() throws Exception {
         assertTrue(result.getResponse().getContentAsString().contains("<input type=\"password\" name=\"password\"/>"));
     }
 }
